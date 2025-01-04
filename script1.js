23b26fd5e78dfa236fed6272a671d57a4449ee89
dolphin <dolphin@crypto.world>
2025-01-04 09:48:37 +0800

    20250104

--- script1.js
+++ script1.js
@@ -30,7 +30,7 @@
                 }
             })
         };
-        handle({ currPath: `C:\\xxx\\20250103` });
+        handle({ currPath: `C:\\xxx\\20250104` });
         handle({ currPath: `C:\\xxx\\20250101` });
     }
 })();
